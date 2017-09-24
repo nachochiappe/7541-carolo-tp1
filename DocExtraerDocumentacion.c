d338ff8e50bd05834269aa7b1f891fa810d22419
Ignacio Chiappe <nacho.chiappe@gmail.com>
2017-09-24 11:42:12 -0300

    Funciona para todos los parámetros que se incluyan. Se está haciendo sin memoria dinámica.

--- DocExtraerDocumentacion.c
+++ DocExtraerDocumentacion.c
@@ -8,14 +8,18 @@
 #define RES_OK 0
 #define RES_ERROR -1
 
+typedef struct t_parametro {
+	char param_nombre[30];
+	char param_valor[100];
+} t_parametro;
+
 typedef struct TDA_Doc {
-	char funcion[50];
+	char funcion[100];
 	char descr[255];
 	char autor[50];
 	char fecha[15];
 	char version[15];
-	char param_nombre[30];
-	char param_valor[100];
+	t_parametro param[10];
 	char devuelve[255];
 	char pre[255];
 	char pos[255];
@@ -37,7 +41,8 @@ int generarCabeceraHTML (FILE *arch_HTML, char *arch_salida) {
 	return (0);
 }
 
-int generarContenidoHTML (FILE *arch_HTML, char *arch_salida, TDA_Doc *tda) {
+int generarContenidoHTML (FILE *arch_HTML, char *arch_salida, TDA_Doc *tda, unsigned char i) {
+
 	arch_HTML = fopen(arch_salida, "a");
 
 	fputs("\t\t\t<h3>Funci&oacute;n: <a name=\"", arch_HTML);
@@ -62,11 +67,13 @@ int generarContenidoHTML (FILE *arch_HTML, char *arch_salida, TDA_Doc *tda) {
 	fputs(tda->version, arch_HTML);
 	fputs("</dd>\n\t\t\t</dl>\n", arch_HTML);
 
-	fputs("\t\t\t<dl>\n\t\t\t\t<dt>Par&aacute;metro: ", arch_HTML);
-	fputs(tda->param_nombre, arch_HTML);
-	fputs("</dt>\n\t\t\t\t<dd>", arch_HTML);
-	fputs(tda->param_valor, arch_HTML);
-	fputs("</dd>\n\t\t\t</dl>\n", arch_HTML);
+	for (unsigned char j = 0; j < i; j++) {
+		fputs("\t\t\t<dl>\n\t\t\t\t<dt>Par&aacute;metro: ", arch_HTML);
+		fputs((tda->param[j]).param_nombre, arch_HTML);
+		fputs("</dt>\n\t\t\t\t<dd>", arch_HTML);
+		fputs((tda->param[j]).param_valor, arch_HTML);
+		fputs("</dd>\n\t\t\t</dl>\n", arch_HTML);
+	}
 
 	fputs("\t\t\t<dl>\n\t\t\t\t<dt>Retorno</dt>\n\t\t\t\t<dd>", arch_HTML);
 	fputs(tda->devuelve, arch_HTML);
@@ -100,6 +107,7 @@ int DocExtraerDocumentacion (TDA_Doc *tda, char *arch_entrada, char *arch_salida
 	char linea[255];
 	char *palabra_res;
 	char *valor;
+	unsigned char i;
 	archivo = fopen(arch_entrada, "r");
 	arch_HTML = fopen(arch_salida, "w");
 	if (generarCabeceraHTML(arch_HTML, arch_salida) != 0) return RES_ERROR;
@@ -109,11 +117,12 @@ int DocExtraerDocumentacion (TDA_Doc *tda, char *arch_entrada, char *arch_salida
 			while (fgets(linea, sizeof(linea), archivo)) {
 				if (strncmp(linea, "/*", 2) == 0) {
 					/* INICIO COMENTARIO */
+					i = 0;
 					while (fgets(linea, sizeof(linea), archivo)) {
 						if ((strncmp(linea, "*/", 2) == 0) || (strncmp(linea, " */", 3) == 0)) {
 							/* FIN COMENTARIO */
 							/* CONSTRUIR HTML */
-							if (generarContenidoHTML(arch_HTML, arch_salida, tda) == 0) break;
+							if (generarContenidoHTML(arch_HTML, arch_salida, tda, i) == 0) break;
 							else return RES_ERROR;
 						}
 						else if (strncmp(linea, "@", 1) == 0) {
@@ -126,9 +135,10 @@ int DocExtraerDocumentacion (TDA_Doc *tda, char *arch_entrada, char *arch_salida
 							else if (strcmp(palabra_res, "@version") == 0) strcpy(tda->version, valor);
 							else if (strcmp(palabra_res, "@param") == 0) {
 								valor = strtok(valor, " ");
-								strcpy(tda->param_nombre, valor);
+								strcpy((tda->param[i]).param_nombre, valor);
 								valor = strtok(NULL, "\0");
-								strcpy(tda->param_valor, valor);
+								strcpy((tda->param[i]).param_valor, valor);
+								i++;
 							}
 							else if (strcmp(palabra_res, "@return") == 0) strcpy(tda->devuelve, valor);
 							else if (strcmp(palabra_res, "@pre") == 0) strcpy(tda->pre, valor);
